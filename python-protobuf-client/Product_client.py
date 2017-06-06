10a001091a85bddab978e086dcbfbeaf9c6c95b5
lijiaming <lijiaming1984@gmail.com>
2017-06-06 21:15:09 +0800

    add some comments

--- python-protobuf-client/Product_client.py
+++ python-protobuf-client/Product_client.py
@@ -1,3 +1,10 @@
+'''
+    1. install protoc python: sudo pip install protorpc
+
+    2. generate python proto stub: protoc -I spring-rest-proto-service/src/main/proto/ --python_out=python-protobuf-client spring-rest-proto-service/src/main/proto/*.proto
+
+    3. import http and proto lib, send http request to service, and parse to string
+'''
 import Product_pb2
 import urllib
 if __name__ == '__main__':
